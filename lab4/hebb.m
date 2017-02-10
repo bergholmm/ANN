f02a86c81578e8ef84d770d6d7a005b9660689c7
bergholmm <marcusbergholm@hotmail.com>
2017-02-10 10:36:03 +0100

    Labb 3 and some  4

--- lab4/hebb.m
+++ lab4/hebb.m
@@ -1,17 +1,31 @@
 clear;
 addpath ./stuff;
 
+nodes = 8;
+
 x1 = vm([0 0 1 0 1 0 0 1]);
 x2 = vm([0 0 0 0 0 1 0 0]);
 x3 = vm([0 1 1 0 0 1 0 1]);
 
 patterns = [x1; x2; x3]';
-W = zeros(8,8);
+W = zeros(nodes, nodes);
+
+N = log(nodes);
 
 for p = patterns
-    W = W + p'.*p - eye(8);
+    W = W + p'.*p - eye(nodes);
 end
               
-disp(t0(sgn(x1*W)))
-disp(t0(sgn(x2*W)))
-disp(t0(sgn(x3*W)))
+%disp(t0(sgn(x1*W)))
+%disp(t0(sgn(x2*W)))
+%disp(t0(sgn(x3*W)))
+
+%x1d = vm([1 0 1 0 1 0 0 1]);
+%x2d = vm([1 1 0 0 0 1 0 0]);
+%x3d = vm([1 1 1 0 1 1 0 1]);
+
+%x = x3d;
+%i = 0;
+
+ x = de2bi(0:2^nodes-1);
+ y = unique(t0(sgn(x*W)), 'rows')
